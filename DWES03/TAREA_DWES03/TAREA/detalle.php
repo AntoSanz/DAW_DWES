a0805d87cb6f46e2dbe22dcea6027b119d58914a
Antonio <aspans@gmail.com>
2024-01-14 19:15:26 +0100

    Archivos principales creados.

---
+++ DWES03/TAREA_DWES03/TAREA/detalle.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio Tema 3: Conjuntos de resultados en MySQLi</title>
+    <!--css para usar Bootstrap  -->
+    <link rel="stylesheet" href="bootstrap-5.3.2/css/bootstrap.min.css" />
+    <script src="bootstrap-5.3.2/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="./script.php"></script>
+    <title>Tarea Tema 3: Detalle</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
