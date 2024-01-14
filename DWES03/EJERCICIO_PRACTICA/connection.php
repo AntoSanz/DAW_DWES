fb5bdf7b9fbadc9aee4c8785b51679206a2fbea8
Antonio <aspans@gmail.com>
2024-01-14 17:34:54 +0100

    3.1.1 Comprobar errores

--- DWES03/EJERCICIO_PRACTICA/connection.php
+++ DWES03/EJERCICIO_PRACTICA/connection.php
@@ -4,5 +4,12 @@
 @$connect = new mysqli('localhost', 'gestor', 'secreto', 'daw_dwes_03_ejercicio');
 print $connect->server_info;
 
+//Comprobar error
+$error = $connect->connect_errno;
+if ($error != null) {
+    echo "<p>Error $error conectando a la base de datos: $connect</p>";
+    die();
+}
+
 //Finalizar conexion con BBDD
 $connect->close();
