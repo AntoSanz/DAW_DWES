ebdf85f3417de6643b84682f78a0ace30c6568c3
Antonio <aspans@gmail.com>
2024-01-15 13:29:18 +0100

    feat(borrar.php, update.pho): conexiones actualizadas

--- DWES03/TAREA_DWES03/TAREA/update.php
+++ DWES03/TAREA_DWES03/TAREA/update.php
@@ -1,3 +1,11 @@
+<?php
+//Establecer conexion
+include './script/conection.php';
+$conn = openConnection();
+
+closeConnection($conn);
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -5,12 +13,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ejercicio Tema 3: Conjuntos de resultados en MySQLi</title>
-    <!--css para usar Bootstrap  -->
     <link rel="stylesheet" href="bootstrap-5.3.2/css/bootstrap.min.css" />
     <script src="bootstrap-5.3.2/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="./script.php"></script>
+    <link rel="stylesheet" href="./styles/styles.css" />
     <title>Tarea Tema 3: Update</title>
 </head>
 
