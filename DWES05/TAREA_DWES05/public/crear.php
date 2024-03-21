b47717b4b30363c671298cba375966533a0c193b
Antonio <aspans@gmail.com>
2024-03-21 13:13:41 +0100

    feat(crear.php, crear_usuario.php): CREAR WORKS

--- DWES05/TAREA_DWES05/public/crear.php
+++ DWES05/TAREA_DWES05/public/crear.php
@@ -3,48 +3,53 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../bootstrap-5.3.2/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../styles/styles.css" />
-    <script src="../bootstrap-5.3.2/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../styles/styles.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear Alumno</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
 
-    <title>Tarea Tema 5: Crear usuario</title>
+    <title>Tarea Tema 5: Crear Alumno</title>
 </head>
 
 <body>
-    <div class="form-box">
-        <form action="crear_usuario.php" method="post">
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="dni" placeholder="">
-                <label for="dni">DNI</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="name" placeholder="">
-                <label for="name">Nombre</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="apellido1" placeholder="">
-                <label for="name">Primer apellido</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="apellido2" placeholder="">
-                <label for="apellido2">Segundo apellido</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="number" class="form-control" id="edad" placeholder="">
-                <label for="edad">Edad</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="number" class="form-control" id="telefono" placeholder="">
-                <label for="telefono">Telefono</label>
-            </div>
-            <div class="d-grid gap-2 col-4 mx-auto">
-                <button class="btn btn-primary" type="submit">Button</button>
-            </div>
-        </form>
+    <div class="container mt-5">
+
+        <div class="form-box">
+            <form action="../php/crear_alumno.php" method="post">
+                <div>
+                    <h2>Añadir alumno</h2>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="dni" name="dni" placeholder="">
+                    <label for="dni">DNI</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="name" name="nombre" placeholder="">
+                    <label for="name">Nombre</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="apellido1" name="apellido1" placeholder="">
+                    <label for="apellido1">Primer apellido</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="apellido2" name="apellido2" placeholder="">
+                    <label for="apellido2">Segundo apellido</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="number" class="form-control" id="edad" name="edad" placeholder="">
+                    <label for="edad">Edad</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="number" class="form-control" id="telefono" name="telefono" placeholder="">
+                    <label for="telefono">Telefono</label>
+                </div>
+                <div class="d-grid gap-2 col-4 mx-auto">
+                    <button class="btn btn-primary" type="submit">Crear</button>
+                </div>
+            </form>
+        </div>
     </div>
+
 </body>
 
 </html>
