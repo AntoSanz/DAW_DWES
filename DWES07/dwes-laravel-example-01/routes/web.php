551254bbdd6cc028d792f170aef588882fecc247
Antonio <aspans@gmail.com>
2024-05-02 11:50:03 +0200

    feat(web.php): Crear ruta con parametro y parametro opcional

--- DWES07/dwes-laravel-example-01/routes/web.php
+++ DWES07/dwes-laravel-example-01/routes/web.php
@@ -9,3 +9,14 @@ Route::get('/', function () {
 Route::get('cursos', function () {
     return 'Bienvenido a la pagina principal de cursos';
 });
+
+
+Route::get('cursos/{curso}/{tema?}', function ($curso, $tema = null) {
+    //Si a la ruta se le proporciona el tema, mostrará el primer mensaje
+    //Si a la ruta no se le proporciona el tema, mostrará el segundo mensaje
+    if ($tema) {
+        return "Bienvenido al curso  $curso, tema $tema";
+    } else {
+        return "Bienvenido al curso  $curso";
+    }
+});
