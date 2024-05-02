a0d8daf7f94e8f5287cf1dc1984fec23dbd5975a
Antonio <aspans@gmail.com>
2024-05-02 11:49:14 +0200

    feat(web.php): Crear ruta simple

--- DWES07/dwes-laravel-example-01/routes/web.php
+++ DWES07/dwes-laravel-example-01/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cursos', function () {
+    return 'Bienvenido a la pagina principal de cursos';
+});
