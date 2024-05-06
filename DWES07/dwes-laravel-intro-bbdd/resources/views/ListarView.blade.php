12ecdaccac62643e3ffc232f5297cc12ceabc2cb
Antonio <aspans@gmail.com>
2024-05-06 13:17:48 +0200

    feat(*): Conexion con mysql y creacionb de la vista listar

---
+++ DWES07/dwes-laravel-intro-bbdd/resources/views/ListarView.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Listar')
+@section('content')
+    <h1>Hello List!</h1>
+@endsection
--- DWES07/dwes-laravel-intro-bbdd/routes/web.php
+++ DWES07/dwes-laravel-intro-bbdd/routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 // Añadir el controlador
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\ListarController;
 
 //Ruta con controlador
 Route::get('/', HomeController::class);
+Route::get('/listar', ListarController::class);
 
 //Grupo de rutas
 Route::controller(CursoController::class)->group(function(){
@@ -14,3 +16,4 @@ Route::controller(CursoController::class)->group(function(){
     Route::get('cursos/create', 'create');
     Route::get('cursos/{curso}/{tema?}', 'show');
 });
+
